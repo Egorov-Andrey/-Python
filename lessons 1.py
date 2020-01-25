d4ed1f52b0431369acc377946d8be7fce9979638
Egorov-Andrey <andreye89@mail.ru>
2020-01-25 13:43:44 +0300

    Практическое задание 1-го урока по курсу: Основы языка Python.

---
+++ lessons 1.py
@@ -0,0 +1,66 @@
+ Задача 1
+
+a = 5
+b = 7
+c = 13
+
+print(c)
+print(b)
+
+a = input("Введите число")
+print(a)
+capital = input("Назовите столицу Австрии")
+print(capital)
+
+задача 2
+
+seconds = input("Введите время в секундах")
+hour = int(seconds) / 3600
+minutes = int(seconds) / 60
+
+print(f'часы: {hour} минуты: {minutes} секунды: {seconds}')
+
+задача 3
+
+n = int(input('Введите число'))
+h = n +
+print(n + nn + nnn)
+
+
+задача 4
+
+a = input('Введите целое положительное число')
+while a < 10
+    print(a)
+    a = a +
+
+задача 5
+
+a = int(input('Какое значение выручки'))
+b = int(input('Какое значение издержек'))
+с = ((a - b)/a)
+if a > b:
+    print("компания работает с прибылью")
+    print(с)
+else:
+    print("компания работает с издержками")
+d = int(input('Сколько сотрудников работает в фирме'))
+print((a - b)/d)
+
+# задача 6
+
+a = int(input())
+b = int(input())
+day = 1
+while b - a > 0:
+    a = a + (a * 0,1)
+    day += 1
+    print(day)
+
+
+
+
+
+
+
+
