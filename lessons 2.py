d6c64347e1449f3a2667e4f990b34fc2b0b00dee
Egorov-Andrey <andreye89@mail.ru>
2020-01-28 23:03:34 +0300

    lessons 2 (3 задачи)

---
+++ lessons 2.py
@@ -0,0 +1,26 @@
+# Задача 1
+
+# list = [6, "red", 456, 45.3, "green", 7]
+#
+# print(type(0))
+# print(type(1))
+# print(type(2))
+# print(type(3))
+# print(type(4))
+# print(type(5))
+
+# Задача 2
+
+# list = [10, 15, 3, 8, 0, 9, 6, 13, 1, 5]
+# result_list.insert(0, 15)
+
+
+# Задача 3
+
+# a = input("Введите месяц")
+# list_dict = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# for весна, зима, лето, осень in list_dict.items () :
+#     print(f "весна", "зима", "лето", "осень")
+
+# Задача 4
+
