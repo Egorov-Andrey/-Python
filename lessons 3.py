b48619653bf28706611f73d3128d2c8e40b37d55
Egorov-Andrey <andreye89@mail.ru>
2020-02-01 15:11:00 +0300

    3 задачи к 3-му уроку

---
+++ lessons 3.py
@@ -0,0 +1,22 @@
+
+# Задача 1
+
+var_1 = input('Введите число')
+var_2 = input('Введите число')
+def func(var_1):
+    def int_func(var_2):
+        return var_1 / var_2
+    return int_func
+
+# Задача 2
+
+def func(Andrey, Egorov, '1989', andreye89@mail.ru)
+    return func
+
+# Задача 3
+
+def my_func(6, 7, 8):
+    return sum(my_func(6, 7, 8))
+
+# Задача 4
+
