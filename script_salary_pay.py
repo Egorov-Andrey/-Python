626b3e1fd9ee231012f5a9b758be69ea952f94b3
Egorov-Andrey <andreye89@mail.ru>
2020-02-08 14:27:14 +0300

    ДЗ к 4-му уроку.

---
+++ script_salary_pay.py
@@ -0,0 +1,59 @@
+#
+# # Задача 1
+# from sys import argv
+#
+# calculation_salary, productivity_hour, hours_p, premium = argv
+#
+# print('Расчет зарплаты', calculation_salary)
+# print('Выработка в час', productivity_hour)
+# print('Часы', hours_p)
+# print('Премия', premium)
+#
+# # Задача 2
+#
+# my_list =  [1, 2, 3, 4, 5]
+# new_list = [el+6 for el in my_list]
+# print(f'Начальный список: {my_list}')
+# print(f'Новый список: {new_list}')
+
+# Задача 3
+
+# generator = (el: el / 20, 21 for el in range (0, 240))
+# print(generator)
+
+# Задача 4
+
+# my_list = (1, 5, 3, 2, 3, 4, 5)
+# my_set =
+
+# Задача 5
+
+# from functools import reduce
+#
+# generator = range (100, 1000)
+#
+# def my_func(generator)
+#     return generator / 2
+# print(reduce(my_func(100, 1000)))
+
+# Задача 6
+
+# from itertools import  count
+#
+# for el in count(78):
+#     print(el)
+
+# from itertools import cycle
+#
+# c=0
+# for el in cycle("Ivan, Andrey, Sergey, Alexcandr")
+#     print(el)
+#     c+=1
+#
+#  # Задача 7
+#
+# def generator():
+#     for el in fibo_gen(n!)
+#         yield el
+# g = generator()
+# print(g)
